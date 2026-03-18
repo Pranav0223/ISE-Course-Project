3e49f098b75c39eaab66a83313e2a8cff640c77e
Vraj <vrajp9136@gmail.com>
2026-03-18 16:12:42 +0530

    added backend setup
    Add auth APIs (login, signup) and user & citizen models

---
+++ Backend/src/models/UserModel.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["viewer", "policy-maker", "admin"],
+      default: "viewer",
+    },
+    department: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("users", userSchema);
